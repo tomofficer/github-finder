e89ff8f93651d9f92a4c694ec4104652a3afdf71
Tom Officer <thomasofficerjr@gmail.com>
2021-10-10 02:41:39 -0700

    Added User Component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 
@@ -15,10 +16,11 @@ const UserItem = ({user: { login, avatar_url, html_url }}) => {
                 />
 
                 <h3>{login}</h3>
+
                 <div>
-                    <a href={html_url} className="but btn-dark btn-sm my-1">
+                    <Link to={`/user/${login}`} className="but btn-dark btn-sm my-1">
                         More
-                        </a>
+                        </Link>
                 </div>
                 </div>
         );

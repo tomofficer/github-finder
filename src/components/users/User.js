6aaf028b1a2114648a72b5f42506b3a1e0ba677a
Tom Officer <thomasofficerjr@gmail.com>
2021-10-10 02:59:21 -0700

    Added functionality and styling to User Component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -20,6 +20,7 @@ export class User extends Component {
 
         const {
             name,
+            company,
             avatar_url,
             location,
             bio,
@@ -42,6 +43,49 @@ export class User extends Component {
                 <Link to='/' className='btn btn-light'>
                     Back To Search
                 </Link>
+                Hireable:{' '}
+                {hireable ? (
+                    <i className='fas fa-check text-success' />
+                ) : (
+                    <i className='fas fa-times-circle text-danger' />
+                )}
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} className="round-img" alt="" style={{ width: '150px' }} />
+                        <h1>{name}</h1>
+                        <p>Location: {location}</p>
+                    </div>
+                    <div>
+                        {bio && (<Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                        )}
+                        <a href={html_url} className="btn btn-dark my-1">
+                            Visit Github Profile
+                        </a>
+                        <ul>
+                            <li>
+                                {login && <Fragment>
+                                    <strong>Username: </strong> {login}
+                                </Fragment>}
+                            </li>
+
+                            <li>
+                                {company && <Fragment>
+                                    <strong>Company: </strong> {company}
+                                </Fragment>}
+                            </li>
+
+                            <li>
+                                {blog && <Fragment>
+                                    <strong>Website: </strong> {blog}
+                                </Fragment>}
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
             </Fragment>
         )
     }

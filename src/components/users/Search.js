5ceac1487763a8c0e3ed1173b7c144f37c18f460
Tom Officer <thomasofficerjr@gmail.com>
2021-10-10 22:32:11 -0700

    Converted from class functions to implement useState and useEffect hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,43 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class Search extends Component {
-    state = {
-        text: ''
-    };
+const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired
-    };
+    const [text, setText] = useState('');
+
     
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if (this.state.text === '') {
-            this.props.setAlert('Please enter something', 'light');
+        if (text === '') {
+            setAlert('Please enter something', 'light');
         } else {
-            this.props.searchUsers(this.state.text);
-            this.setState({ text: '' });
+            searchUsers(text);
+            setText('');
         }
     };
 
-    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+    const onChange = (e) => setText(e.target.value);
 
-    render() {
-
-        const { showClear, clearUsers } = this.props;
-
+ 
         return (
             <div>
-                <form onSubmit={this.onSubmit} className="form">
+                <form onSubmit={onSubmit} className="form">
                     <input 
                         type="text" 
                         name="text" 
                         placeholder="Search Users..." 
-                        value={this.state.text} 
-                        onChange={this.onChange}
+                        value={text} 
+                        onChange={onChange}
                     />
                     <input 
                         type="submit" 
@@ -54,7 +44,13 @@ export class Search extends Component {
                 
             </div>
         )
-    }
 }
+
+Search.propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
+    };
 
 export default Search
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,26 +1,18 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
-export class User extends Component {
+const User = ({ user, loading, getUser, getUserRepos, repos, match  }) => {
 
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        //eslint-disable-next-line
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired
-    };
-
-    render() {
 
         const {
             name,
@@ -36,11 +28,10 @@ export class User extends Component {
             public_repos,
             public_gists,
             hireable
-        } = this.props.user;
+        } = user;
 
-        const { loading, repos } = this.props;
 
-        if (loading) return <Spinner />
+        if (loading) return <Spinner />;
 
         return (
             <Fragment>
@@ -101,7 +92,14 @@ export class User extends Component {
                 <Repos repos={repos} />
             </Fragment>
         )
-    }
 }
+
+User.propTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
+        getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired
+    };
 
 export default User
